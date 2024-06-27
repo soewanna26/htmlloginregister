a06f87cea799822e00b0cae20e52a21166e0101b
soewanna26 <mgsoewana26@gmail.com>
2024-06-27 11:37:39 +0630

    Login Register

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,3 +25,28 @@ const showHiddenPass = (loginPass, loginEye) => {
 };
 
 showHiddenPass("login-pass", "login-eye");
+const showHiddenPass1 = (loginPass, loginEye) => {
+  const input = document.getElementById(loginPass),
+    iconEye = document.getElementById(loginEye);
+
+  iconEye.addEventListener("click", () => {
+    //Change password to text
+    if (input.type === "password") {
+      //switch to text
+      input.type = "text";
+
+      //Icon change
+      iconEye.classList.add("ri-eye-line");
+      iconEye.classList.remove("ri-eye-off-line");
+    } else {
+      //switch to password
+      input.type = "password";
+
+      //Icon change
+      iconEye.classList.remove("ri-eye-line");
+      iconEye.classList.add("ri-eye-off-line");
+    }
+  });
+};
+
+showHiddenPass1("login-pass1", "login-eye1");
